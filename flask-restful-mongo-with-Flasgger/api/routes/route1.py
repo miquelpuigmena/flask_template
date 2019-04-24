8b3aa3266ad60a21aa175a557b0e28dfb9ffccf8
miquel <miquelpuigmena@gmail.com>
2019-04-24 14:22:48 +0200

    Moudlarized swagger yaml files

--- flask-restful-mongo-with-Flasgger/api/routes/route1.py
+++ flask-restful-mongo-with-Flasgger/api/routes/route1.py
@@ -1,5 +1,6 @@
 import json
 from http import HTTPStatus
+from flasgger.utils import swag_from
 from flask import Blueprint, request, abort
 from flask_restful import Resource
 from logger.logger import LoggerHelper
@@ -10,72 +11,15 @@ class Route1(Resource):
 
     LOGGER = LoggerHelper.get_logger(name=__name__, filename=__name__ + ".log")
 
+    @swag_from("/swagger/route1/get.yaml")
     def get(self, id='No param provided'):
-        """
-        Get a nice string dependant on id.
-        Retrieve: GET Inside route1 - {id}.
-        Parse id param and form phrase.
-        If not id provided, by default id='No param provided'.
-        ---
-        tags:
-          - Route1
-        parameters:
-          - in: path
-            name: id
-            type: string
-            required: false
-        produces:
-          - "plain/text"
-        responses:
-          202:
-            description: Request succeeded
-          400:
-            description: Internal server error
-        """
         try:
             return "GET Inside route1 - " + str(id), HTTPStatus.ACCEPTED
         except Exception as ex:
             abort(HTTPStatus.BAD_REQUEST, str(ex))
 
+    @swag_from("/swagger/route1/post.yaml")
     def post(self):
-        """
-        Creates a new BasicModel1 and saves it!
-        Query contains a Json object.
-        Dummy post to create a simple model and storing in DB.
-        Query must include a json data which will be returned in response with a 'response'
-        field added or overwritten.
-        ---
-        tags:
-          - Route1
-        consumes:
-          - application/json
-        parameters:
-          - in: body
-            name: dummy_json
-            required: true
-            schema:
-              type: "object"
-              properties:
-                dummy-field:
-                  type: string
-                example:
-                  dummy-field: dummy-value
-        responses:
-          202:
-            description: Post succeeded
-            schema:
-              type: "object"
-              properties:
-                dummy-field:
-                  type: string
-            response:
-              type: string
-              example:
-                dummy-field: dummy-value
-                response: ModelReferenced1 successfully stored
-          400:
-            description: Bad request
-        """
         try:
             body = request.get_json(force=True)
             BasicModel1(ref_param1="uno", ref_param2="dos").save()
--- flask-restful-mongo-with-Flasgger/api/routes/route2.py
+++ flask-restful-mongo-with-Flasgger/api/routes/route2.py
@@ -1,5 +1,6 @@
 import json
 from http import HTTPStatus
+from flasgger.utils import swag_from
 from flask import Blueprint, request, abort
 from flask_restful import Resource
 from logger.logger import LoggerHelper
@@ -9,71 +10,15 @@ class Route2(Resource):
 
     LOGGER = LoggerHelper.get_logger(name=__name__, filename=__name__ + ".log")
 
+    @swag_from("/swagger/route2/get.yaml")
     def get(self, id='No param provided'):
-        """
-        Get a nice string dependant on id.
-        Retrieve: GET Inside route2 - {id}.
-        Parse id param and form phrase.
-        If not id provided, by default id='No param provided'.
-        ---
-        tags:
-          - Route2
-        parameters:
-          - in: path
-            name: id
-            type: string
-            required: false
-        produces:
-          - "plain/text"
-        responses:
-          202:
-            description: Request succeeded
-          400:
-            description: Internal server error
-        """
         try:
-            return "GET Inside route2 - " + str(id), HTTPStatus.ACCEPTED
+            return json.dumps({id: "GET Inside route2"}), HTTPStatus.ACCEPTED
         except Exception as ex:
             abort(HTTPStatus.BAD_REQUEST, str(ex))
 
+    @swag_from("/swagger/route2/post.yaml")
     def post(self):
-        """
-        Dummy post on route2.
-        Query contains a Json object.
-        Query must include a json data which will be returned in response with a 'response'
-        field added or overwritten.
-        ---
-        tags:
-          - Route2
-        consumes:
-          - application/json
-        parameters:
-          - in: body
-            name: dummy_json
-            required: true
-            schema:
-              type: "object"
-              properties:
-                dummy-field:
-                  type: string
-                example:
-                  dummy-field: dummy-value
-        responses:
-          202:
-            description: Post succeeded
-            schema:
-              type: "object"
-              properties:
-                dummy-field:
-                  type: string
-            response:
-              type: string
-              example:
-                dummy-field: dummy-value
-                response: POST Inside route2
-          400:
-            description: Bad request
-        """
         try:
             body = request.get_json(force=True)
             body["response"] = "POST Inside route2"
